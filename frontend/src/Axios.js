f122bad13b1716e3b864fa421874a400a0e5e3d7
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-04-13 19:29:54 +0530

    follow detail added

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,3 +1,3 @@
-// export const baseURL = "https://twiktik.onrender.com"
+export const baseURL = "https://twiktik.onrender.com";
 
-export const baseURL = "http://localhost:5000";
+// export const baseURL = "http://localhost:5000";
